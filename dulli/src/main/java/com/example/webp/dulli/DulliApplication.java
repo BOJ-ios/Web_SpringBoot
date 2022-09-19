e16219ee8f28c5d0c4cf5a2dc42878548549650b
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-09-19 19:27:55 +0900

    9-19

---
+++ dulli/src/main/java/com/example/webp/dulli/DulliApplication.java
@@ -0,0 +1,11 @@
+package com.example.webp.dulli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DulliApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DulliApplication.class, args);
+	}
+}
