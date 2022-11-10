1eeabb6ec625151eb9c4fdd30b55b48b2e084b59
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-11-10 23:11:47 +0900

    11-10

---
+++ p5/src/main/java/com/example/sql/p5/Star.java
@@ -0,0 +1,12 @@
+package com.example.sql.p5;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Star {
+  @Id
+  public Integer no;
+  public String name;
+  public Integer count;
+}
--- p5/src/main/java/com/example/sql/p5/VanController1.java
+++ p5/src/main/java/com/example/sql/p5/VanController1.java
@@ -1,6 +1,9 @@
 package com.example.sql.p5;
 
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
@@ -80,5 +83,82 @@ public class VanController1 {
     List<Memo> list = memo_Repository.findAll(Sort.by(Sort.Direction.ASC, "wdate"));
     mo.addAttribute("list", list);
     return "/week9_homework/memoList";
+  }
+
+  // foods
+  @GetMapping("/foods")
+  public String foods() {
+    return "foods";
+  }
+
+  @PostMapping("/foods/result")
+  public String foodsResult(String gender, int age, String[] arr,
+      int face, int grade, int omg, String comments, Model mo) {
+    mo.addAttribute("gender", gender);
+    mo.addAttribute("age", age);
+    String temp = "";
+    if (arr == null)
+      temp = "없음";
+    else {
+      StringBuilder sb = new StringBuilder();
+      int length = arr.length;
+      for (int i = 0; i < length; i++) {
+        sb.append(arr[i]);
+        if (i != length - 1) {
+          sb.append(", ");
+        }
+      }
+      temp = sb.toString();
+    }
+    mo.addAttribute("foods", temp);
+    mo.addAttribute("face", face);
+    mo.addAttribute("grade", grade);
+    mo.addAttribute("omg", omg);
+    mo.addAttribute("comments", comments);
+    return "foodsResult";
+  }
+
+  // star
+  @Autowired
+  private StarRepository starRep;
+
+  @GetMapping("/star/1")
+  public String star1(Model mo) {
+    String choice1_1 = "테슬라 모델 3";
+    String choice1_2 = "아이오닉 6";
+    mo.addAttribute("choice1_1", choice1_1);
+    mo.addAttribute("choice1_2", choice1_2);
+    return "star1";
+  }
+
+  @GetMapping("/star/2")
+  public String star2(HttpSession session, Model mo, String choice1) {
+    session.setAttribute("choice1", choice1);
+    String choice2_1 = "포르쉐 타이칸";
+    String choice2_2 = "벤츠 EQE";
+    mo.addAttribute("choice2_1", choice2_1);
+    mo.addAttribute("choice2_2", choice2_2);
+    return "star2";
+  }
+
+  @GetMapping("/star/3")
+  public String star3(HttpSession session, Model mo, String choice2) {
+    mo.addAttribute("choice1", session.getAttribute("choice1"));
+    mo.addAttribute("choice2", choice2);
+    return "star3";
+  }
+
+  @GetMapping("/star/winner")
+  public String starWinner(String winner, Model mo) {
+    // starRep.updateCount(winner);
+    mo.addAttribute("winner", winner);
+    return "starWinner";
+  }
+
+  @GetMapping("/star/list")
+  public String starList(Model mo) {
+    List<Star> list = starRep.findAll();
+    mo.addAttribute("list", list);
+    return "starList";
   }
 } // Controller class
